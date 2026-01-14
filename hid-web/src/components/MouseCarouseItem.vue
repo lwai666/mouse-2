4f8da0880dafe0c6fc9950936a826d35ce1f530e
邢峻豪 <xingjunhao@yazuishou.com>
2026-01-14 17:29:44 +0800

    fix: 位置更换

--- hid-web/src/components/MouseCarouseItem.vue
+++ hid-web/src/components/MouseCarouseItem.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
-
-
 const props = withDefaults(defineProps(), { width: '50%' })
 const { t } = useI18n()
 const language = inject('language')
 // const transport = inject<Ref<TransportWebHIDInstance>>('transport');
-
-
 
 watch(() => language, () => {
   // const constants = useConstants(t)
@@ -16,48 +12,42 @@ watch(() => language, () => {
   // })
   // options.value = deepClone(constants.mouseKeyOptions)
 }, { immediate: true, deep: true })
-
-
-
-
-
-
 </script>
 
 <template>
-  <div   class="w-100% flex justify-between items-center" style="flex-direction: column;margin-top: 20px;">
-    <div class="flex justify-between items-center mb-10px" style="width: 70%;">
-        <img src="/public/mouseCarouseItem/1.png" alt="" srcset="">
-        <img style="height: 1px;" src="/public/mouseCarouseItem/line.png" alt="" srcset="">
-        <img src="/public/mouseCarouseItem/2.png" alt="" srcset="">
-        <img style="height: 1px;" src="/public/mouseCarouseItem/line.png" alt="" srcset="">
-        <img src="/public/mouseCarouseItem/3.png" alt="" srcset="">
+  <div class="w-100% flex items-center justify-between" style="flex-direction: column;margin-top: 20px;">
+    <div class="mb-10px flex items-center justify-between" style="width: 70%;">
+      <img src="/public/mouseCarouseItem/1.png" alt="" srcset="">
+      <img style="height: 1px;" src="/public/mouseCarouseItem/line.png" alt="" srcset="">
+      <img src="/public/mouseCarouseItem/2.png" alt="" srcset="">
+      <img style="height: 1px;" src="/public/mouseCarouseItem/line.png" alt="" srcset="">
+      <img src="/public/mouseCarouseItem/3.png" alt="" srcset="">
     </div>
     <div class="flex justify-between" style="width: 86%;margin-top: 20px;">
-      <div class="flex justify-between items-center" style="flex-direction: column;width: 300px;">
-          <div style="margin-bottom: 5px;">
-            <p >{{ t('description.mouse_pairing_instructions1') }}</p>
-            <p >({{ t('description.mouse_pairing_instructions1_sub') }})</p>
-          </div>
-          <img src="/public/mouseCarouseItem/icon1.png" alt="" srcset="">
-      </div>
-
-      <div  class="flex justify-between items-center" style="flex-direction: column;width: 300px;">
+      <div class="flex items-center justify-between" style="flex-direction: column;width: 300px;">
         <div style="margin-bottom: 5px;">
-          <p >{{ t('description.mouse_pairing_instructions2') }}</p>
-          <p style="margin-bottom: 5px;">{{ t('description.mouse_pairing_instructions2_sub') }}</p>
+          <p>{{ t('description.mouse_pairing_instructions1') }}</p>
+          <p>({{ t('description.mouse_pairing_instructions1_sub') }})</p>
         </div>
-          
-          <img src="/public/mouseCarouseItem/icon2.png" alt="" srcset="">
+        <img src="/public/mouseCarouseItem/icon1.png" alt="" srcset="">
       </div>
-      <div  class="flex justify-between items-center" style="flex-direction: column;width: 300px;">
+      <div class="flex items-center justify-between" style="flex-direction: column;width: 300px;">
         <div style="margin-bottom: 5px;">
-          <p >{{ t('description.mouse_pairing_instructions3') }}</p>
-          <p style="margin-bottom: 5px;">{{ t('description.mouse_pairing_instructions3_sub') }}</p>
+          <p>{{ t('description.mouse_pairing_instructions3') }}</p>
+          <p style="margin-bottom: 5px;">
+            {{ t('description.mouse_pairing_instructions3_sub') }}
+          </p>
         </div>
-
-          
-          <img src="/public/mouseCarouseItem/icon3.png" alt="" srcset="">
+        <img src="/public/mouseCarouseItem/icon3.png" alt="" srcset="">
+      </div>
+      <div class="flex items-center justify-between" style="flex-direction: column;width: 300px;">
+        <div style="margin-bottom: 5px;">
+          <p>{{ t('description.mouse_pairing_instructions2') }}</p>
+          <p style="margin-bottom: 5px;">
+            {{ t('description.mouse_pairing_instructions2_sub') }}
+          </p>
+        </div>
+        <img src="/public/mouseCarouseItem/icon2.png" alt="" srcset="">
       </div>
     </div>
   </div>
